d8343596b372bdc514ed2b83653886a95b7d93b8
Nael789 <natanmoreira890@gmail.com>
2024-05-11 16:21:57 -0300

    Add files via upload
    inicio do curso

---
+++ Logica-de-Progamacao/src/exercicios/execicio.java
@@ -0,0 +1,32 @@
+package exercicios;
+
+import java.sql.SQLOutput;
+
+public class execicio {
+
+      private static double media;
+
+      public static void main(String[] args){
+      String gameRPG, gameIndice;
+      int age, code;
+      char gender;
+      double princeRPG, princeIndice =  media;
+
+      gameRPG = "Hogwarts Legacy;" +
+                "priceRPG = 107.80;";
+
+      gameIndice = "Hollow Knight";
+               princeIndice = 46.99;
+
+               age = 35;
+               code = 2367;
+               gender = 'm';
+
+               media = (princeRPG = princeIndice) / 2;
+
+            System.out.println("Produtos");
+            System.out.println("gameRPG = ", está custando R$ );
+
+      }
+}
+
---
+++ Logica-de-Progamacao/src/tipos/primitivos/Inicio.java
@@ -0,0 +1,18 @@
+package tipos.primitivos;
+
+public class Inicio {
+
+   static int idadeUsuario = 50;
+   static String nomeUsuario = "Natanael";
+   static float alturaUsuario = 1.75F;
+
+   public static void main(String[] args) {
+      System.out.println("Olá Mundo");
+      System.out.println("Nome" + nomeUsuario);
+      System.out.println("idade" + idadeUsuario);
+      System.out.println("altura" + alturaUsuario);
+   }
+}
+
+
+
